428bdc9f43fb5543042ea4ff1ceed7d8f81f658f
aalacy <aaronlacywhite387@gmail.com>
2019-11-16 11:54:35 -0800

    add pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 module.exports = {
   apps: [
     {
-      name: 'MyApp',
+      name: 'Avenue',
       script: 'server.js',
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-      instances: 2,
+      instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G'
