82f054ea8db5bde1445825ff93263eea117529c2
aalacy <aaronlacywhite387@gmail.com>
2020-03-12 12:03:23 -0700

    fix issue on proeprty controller

--- app/controllers/property.js
+++ app/controllers/property.js
@@ -234,7 +234,7 @@ exports.overview = async function(req, res) {
   const expenses_percent = total_cost > 0 ? (Math.abs(total_expenses)/total_income*100).toFixed(2) : 0;
   const operating_expense_ratio = expenses_percent;
   const gross_yield = (total_income/parseFloat(property.current_value)*100).toFixed(2);
-  const net_yield = 0;
+  let net_yield = 0;
   if (property.purchase_price > 0) {
     net_yield = (net_profit/parseFloat(property.purchase_price)*100).toFixed(2);
   }
